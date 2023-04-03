a064226100d47969ded309b364428515e2e9c415
mcscott5 <mcscott5@crimson.ua.edu>
2023-04-03 15:05:29 -0500

    Adding times

--- Program.cs
+++ Program.cs
@@ -3,17 +3,22 @@
 Trainer mike = new Trainer("12", "Mike Jones", "1401 Snow Hinton Dr", "mjones@gmail.com");
 System.Console.WriteLine(mike.GetMailingAddress());
 
+TimeOnly opening = new TimeOnly(8, 0);
+TimeOnly closing = new TimeOnly(17, 0);
 
-
-Listing aList = new Listing("12", "Mike Jones", "4/3/2023", "11:30 PM", 30.50, "Cancelled");
+Listing aList = new Listing("12", "Mike Jones", "4/3/2023", "11:30 AM", 30.50, "Cancelled");
 
 StreamWriter outFile = new StreamWriter("out.txt");
 outFile.WriteLine(aList.ToFile());
 System.Console.WriteLine(aList.ToString());
 System.Console.WriteLine(aList.GetDate());
 System.Console.WriteLine(aList.GetTime());
+aList.SetTime(TimeOnly.Parse(aList.GetTime()).AddHours(1).ToString());
+System.Console.WriteLine(aList.GetTime()); 
 outFile.Close();
-
+if(TimeOnly.Parse(aList.GetTime()).IsBetween(opening, closing)){
+    System.Console.WriteLine("We're open!");
+}
 // DateTime today = DateTime.Today;
 // System.Console.WriteLine(today.ToShortDateString());
 // System.Console.WriteLine(today.ToShortTimeString());
